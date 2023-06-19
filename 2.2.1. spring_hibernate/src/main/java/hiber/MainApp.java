cdf1e98c79875ca4440c765373dad9ce550324ed
@KolibanSergey <koliban171@gmail.com>
2023-06-19 13:09:33 +0300

    2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,40 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        userService.add(new Car("AUDI", 10));
+        userService.add(new Car("BMW", 11));
+        userService.add(new Car("Lada", 12));
+        userService.add(new Car("Lada", 33));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
 
-      context.close();
-   }
+        List<Car> cars = userService.listCars();
+
+        System.out.println("\n=== Добавление пользователей с машинами в БД");
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru", cars.get(0)));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru", cars.get(1)));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru", cars.get(2)));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru", cars.get(3)));
+        System.out.println("\n=== ok");
+
+        System.out.println("\n=== Select пользователя с машиной");
+        User user = userService.findOwner("AUDI", 10);
+        System.out.println(user);
+        System.out.println("\n=== ок");
+
+        context.close();
+    }
 }
